d46a16d2cb3afc4c8aa8b4f01cafbe17bd4a78cf
ZinMarLwin <zimmar07@gmail.com>
2026-05-23 11:13:57 +0900

    Item Loop

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ function Item({name}){
   return <li>{name}</li>;
 }
 
+const data = [
+  {id:3, name:"Bread",done:false},
+  {id:2, name:"Butter",done:true},
+  {id:1, name:"Banana",done:false},
+];
+
 export default function App() {
     return(
        <div>
         <h1>Hello React</h1>
         <ul>
-          <Item name="Apple" />
-          <Item name="Orange" />
-          <Item name="Egg" />
+          {data.map(item => {
+            return <Item name={item.name}/>
+          })}
+
+          {data.map(item => <Item name={item.name}/>)}
+
+          {data.map(item => (
+            <Item name={item.name}/>
+          ))}
         </ul>
     </div>
     );
